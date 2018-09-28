55f052dfc884e7d502a5be0882225bceec69b4ae
gongdapeng <gongdapeng@bytedance.com>
2018-09-28 15:37:57 +0800

    fix: 解决 X-Frame-Options 导致加载失败问题

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # Chrome Extension for Screen Split
 
-Chrome 分屏插件，基于 Frameset 加载两个相同 url 的 Frame 实现。（核心代码只有一句，忘了是从知乎还是 StackOverflow 复制而来...在原有书签的基础上换了一种展现形式）
+Chrome 分屏插件，基于 Frameset 加载两个相同 url 的 Frame 实现。（核心代码只有一句，忘了是从知乎还是 StackOverflow 复制而来...在原有书签的基础上换了一种展现形式）。
 
 ## Usage
 
 - `git clone` 源代码。
-- 打开 Chrome，再“扩展程序”设置中选择“加载已解压的扩展程序”即可。
+- 打开 Chrome，在“扩展程序”设置中选择“加载已解压的扩展程序”即可。
 - 打开任意网页，点击插件图标即可启用。
 
 ## Screenshot
 
 ![截图](./assets/screenshot.png)
+
+## Todo
+
+- [ ] 解决 CSP 导致加载失败问题。
+
+## ChangeLog
+
+- 2018-09-28：解决因为 `X-Frame-Options` 导致分屏失败。
+
 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function (tab) {
   const script = `document.write("<HTML><HEAD></HEAD><FRAMESET COLS=\'50%25,*\'><FRAME src='${tab.url}' /><FRAME src='${tab.url}' /></FRAMESET></HTML>")`;
 
   // TODO: 有的页面如 chrome://extensions 执行无效
@@ -6,3 +6,18 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     code: script,
   });
 });
+
+chrome.webRequest.onHeadersReceived.addListener(
+  function (details) {
+    for (var i = 0; i < details.responseHeaders.length; ++i) {
+      // 移除响应头的 x-frame-options 以使页面可以展示在 frame 中。
+      if (details.responseHeaders[i].name.toLowerCase() == 'x-frame-options') {
+        details.responseHeaders.splice(i, 1);
+      }
+    }
+    return {
+      responseHeaders: details.responseHeaders,
+    };
+  }, {
+    urls: ["<all_urls>"]
+  }, ["blocking", "responseHeaders"]);
